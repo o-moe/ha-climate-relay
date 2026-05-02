514319e5457a8dfb805d641fe565c185a98b8081
o-moe <rotbraun_geraete.08@icloud.com>
2026-05-02 22:40:12 +0200

    Blur number inputs in Epic acceptance

--- scripts/run_epic_acceptance.py
+++ scripts/run_epic_acceptance.py
@@ -18,7 +18,7 @@ from urllib.request import Request, urlopen
 DEFAULT_BASE_URL = "http://haos-test.local:8123"
 TOKEN_ENV_VAR = "HOME_ASSISTANT_TOKEN"
 EPIC_1_ACCEPTANCE_VERSION = "v0.1.0-alpha.21"
-EPIC_2_ACCEPTANCE_VERSION = "v0.2.0-alpha.22"
+EPIC_2_ACCEPTANCE_VERSION = "v0.2.0-alpha.23"
 LOCAL_ENV_FILE = Path(".env.local")
 DEFAULT_ARTIFACT_DIR = Path("artifacts") / "acceptance"
 EPIC_2_PRIMARY_CLIMATES = (
@@ -872,6 +872,8 @@ async function setNumberInput(selectorIndex, value) {{
       }}));
     }}
   }}, String(value));
+  await page.keyboard.press("Tab");
+  await page.waitForTimeout(500);
 }}
 
 async function clearNumberInput(selectorIndex) {{
